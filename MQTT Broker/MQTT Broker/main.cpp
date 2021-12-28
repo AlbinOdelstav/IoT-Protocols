f403fde47d0ddd05371362ff3df76ad647398bf3
AlbinOdelstav <albin.odelstav@outlook.com>
2021-12-28 16:32:36 +0100

    Worked on connect, subscribe and unsubscribe

--- MQTT Broker/MQTT Broker/main.cpp
+++ MQTT Broker/MQTT Broker/main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <bitset>
-#include <vector>
 #include "mqtt_broker.h"
 
 int main() {
 	MqttBroker().start(1883);
-
-	std::string hello = "vad fan";
-	std::string data(hello.begin(), hello.begin() + 7);
-	std::cout << data << "\n";
-	system("pause");
 }
